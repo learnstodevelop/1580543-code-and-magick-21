50e6b5f393b08d8a221c9f224f2961779785ef56
Вадим <vba.sm@mail.ru>
2020-10-05 19:04:02 +0700

    Доработка 2 - 9. Учебный проект: одеть Надежду

--- js/setup.js
+++ js/setup.js
@@ -2,15 +2,47 @@
 
 (function () {
   const TOTAL_WIZARDS = 4;
-  const WIZARD_FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор',
-    'Юлия', 'Люпита', 'Вашингтон'];
-  const WIZARD_LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко',
-    'Топольницкая', 'Нионго', 'Ирвинг'];
-  const EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
-  const COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)',
-    'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-  const FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0',
-    '#e848d5', '#e6e848'];
+  const WIZARD_FIRST_NAMES = [
+    'Иван',
+    'Хуан Себастьян',
+    'Мария',
+    'Кристоф',
+    'Виктор',
+    'Юлия',
+    'Люпита',
+    'Вашингтон'
+  ];
+  const WIZARD_LAST_NAMES = [
+    'да Марья',
+    'Верон',
+    'Мирабелла',
+    'Вальц',
+    'Онопко',
+    'Топольницкая',
+    'Нионго',
+    'Ирвинг'
+  ];
+  const EYES_COLORS = [
+    'black',
+    'red',
+    'blue',
+    'yellow',
+    'green'];
+  const COAT_COLORS = [
+    'rgb(101, 137, 164)',
+    'rgb(241, 43, 107)',
+    'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)',
+    'rgb(215, 210, 55)',
+    'rgb(0, 0, 0)'
+  ];
+  const FIREBALL_COLORS = [
+    '#ee4830',
+    '#30a8ee',
+    '#5ce6c0',
+    '#e848d5',
+    '#e6e848'
+  ];
   const MIN_NAME_LENGTH = 2;
   const MAX_NAME_LENGTH = 25;
 
